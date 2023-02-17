517bf52a0b8f14ba6da98db3a3edca0405723d4f
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-02-17 19:35:33 +0330

    fix height page loadding

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 const Loading = () => {
     return (
-      <div className="flex justify-center items-center p-8">
+      <div className="flex justify-center items-center p-8 h-[70vh]">
         <Image
           width={150}
           height={150}
           alt="is run loading....."
           src={'/loading.svg'}
-          />
+        />
       </div>
     );
 }
