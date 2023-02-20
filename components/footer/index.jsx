a1039c0f20b45ac1dfe514819dcc01c893da7171
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-02-20 16:43:24 +0330

    icone footer scoial

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,9 +1,20 @@
+import { BsTelegram } from 'react-icons/bs';
+import { BsWhatsapp } from 'react-icons/bs';
+import { BsInstagram } from 'react-icons/bs';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { AiTwotoneMail } from 'react-icons/ai';
 const Footer = () => {
-    return (
-      <footer className="h-[20vh] wx-[100vw] bg-blue-500 flex justify-center items-center text-cyan-50 p-2 lg:text-[2rem]">
-        این وب سایت آزمایشی می باشد....
-      </footer>
-    );
-}
+  return (
+    <footer className="h-[20vh] wx-[100vw] bg-blue-500 flex justify-center items-center text-cyan-50 p-2 lg:text-[2rem]">
+      <div className="flex gap-5">
+        <AiFillLinkedin />
+        <AiTwotoneMail />
+        <BsWhatsapp />
+        <BsTelegram />
+        <BsInstagram />
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
